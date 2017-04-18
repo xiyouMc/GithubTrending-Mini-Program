301754b471a618521a70393e4e3e18d165fa73d3
Mc <694449212@qq.com>
2017-04-18 10:20:27 +0800

    update

--- Trending/trending_html_parse.py
+++ Trending/trending_html_parse.py
@@ -3,6 +3,7 @@ from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 import threading,common,time,os
 def capture(url, save_fn="capture.png"):
     print url
+    os.environ["PATH"] += ":/usr/local/bin/geckodriver"
     binary = FirefoxBinary('/usr/local/bin/geckodriver')
     browser = webdriver.Firefox(firefox_binary=binary)
     browser.set_window_size(1200, 900)
