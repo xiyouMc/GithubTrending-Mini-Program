4f573f9c6d6363a425a9746638b0d91af1ea0e50
Mc <694449212@qq.com>
2017-05-23 11:18:54 +0800

    fix bug

--- Trending/core/_startup_trending.py
+++ Trending/core/_startup_trending.py
@@ -97,7 +97,7 @@ class Repos:
                 _json = requests.get(github_url,verify=False,headers=header)
                 _j = json.loads(_json.text)
                 if _j.get('message') == 'Not Found':
-                     github_url = github_url.replace('README.md','README.rst')
+                     github_url = github_url.replace('ReadMe.md','README.rst')
                     _json = requests.get(github_url,verify=False,headers=header)
         with open(dirs + '/' + _get_time() + url_md5,'w') as f:
             f.write(_json.text.encode('utf-8'))
