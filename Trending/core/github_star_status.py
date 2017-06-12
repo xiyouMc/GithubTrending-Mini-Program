d9f675babf6292023884d2193106d9087adcb36d
xiyouMc <xiyoumc.dev@gmail.com>
2017-06-12 15:30:09 +0800

    多线程 mult

--- Trending/core/github_star_status.py
+++ Trending/core/github_star_status.py
@@ -6,6 +6,24 @@ import re
 import json
 import time
 import threading
+import multiprocessing
+
+def status(repo,s):
+        repo_content = s.get('https://github.com/' + repo,verify=False)
+        is_star = re.findall('starring-container on',repo_content.text)
+        print is_star
+        if not is_star:
+            stared = 'not_star'
+        else:
+            stared = 'stared'
+        print stared
+        responseStared =  ResponseStar()
+        responseStared.repo = repo
+        responseStared.stared =stared
+        # return 
+        # _repos.append(responseStared.__dict__)
+        return responseStared.__dict__
+
 class ResponseStar:
     def __init__(self):
         self.repo = ''
@@ -15,37 +33,28 @@ class GithubStarStatus:
     def GET(self):
         print 'Star'
         time1 = time.time()
-        self.s = requests.Session()
+        s = requests.Session()
         params = getInput(web.input())
         repos = params.get('githubs')
         repos = repos.replace('&quot;','"')
         print repos
         _json_repos = json.loads(repos)
         username = params.get('username')
-        self.s.cookies = read_cookies(username)
-        self._repos = []
-        threads = []
-        self.repo_size = len(_json_repos)
+        s.cookies = read_cookies(username)
+        _repos = []
+        pool = multiprocessing.Pool(processes=10)
+
         for repo in _json_repos:
-            t = threading.Thread(target=self.status,args=(repo,))
-            threads.append(t)
-        for t in threads:
-            t.setDaemon(True)
-            t.start()
-        time.sleep(2)
-        _json_resp = json.dumps(self._repos)
+            _repos.append(pool.apply_async(status,(repo,s,)).get())
+            # t = threading.Thread(target=self.status,args=(repo,))
+            # threads.append(t)
+        # for t in threads:
+        #     t.setDaemon(True)
+        #     t.start()
+        # time.sleep(2)
+        pool.close()
+        _json_resp = json.dumps(_repos)
+        pool.join()
+        
         print _json_resp,time.time() - time1
         return _json_resp
-    def status(self,repo):
-        repo_content = self.s.get('https://github.com/' + repo,verify=False)
-        is_star = re.findall('starring-container on',repo_content.text)
-        print is_star
-        if not is_star:
-            stared = 'not_star'
-        else:
-            stared = 'stared'
-        responseStared =  ResponseStar()
-        responseStared.repo = repo
-        responseStared.stared =stared
-        self._repos.append(responseStared.__dict__)
-        self.repo_size = self.repo_size-1
