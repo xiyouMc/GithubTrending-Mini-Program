bcf42e3cd30bfedf8143d2ecb94c5e9731445948
xiyouMc <xiyoumc.dev@gmail.com>
2017-08-01 11:02:11 +0800

    add log

--- Trending/core/_repo.py
+++ Trending/core/_repo.py
@@ -14,7 +14,9 @@ class Repos:
                 c = f.readline()
             if not c == None:
                 return c
+        print("start requests")
         _json = requests.get(github_url,verify=False)
+        print("response:" + _json.text)
         if 'README.md' in github_url:
             github_url = github_url.replace('README.md','')
             _json = requests.get(github_url,verify=False)
